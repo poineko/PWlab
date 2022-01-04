07a121cc565ae86e0f4362b7fc59ddea7997cebc
poineko <eliteisan@gmail.com>
2022-01-04 19:41:30 +0700

    Latihan 5.3. Melakukan operasi sederhana dengan operator perbandingan

--- Tugas5/learn.js
+++ Tugas5/learn.js
@@ -8,13 +8,51 @@ console.log(myNumber)
 console.log(myString)
 console.log(myBoolean)
 console.log(phi)
+console.log('________________')
 
 //Latihan 5.2. Melakukan operasi sederhana dengan operator aritmatika
+
 var a = 3, b = 6;
-console.log( a - b );
-console.log( 2 - a - b );
-console.log('3 + 6 =', a + b);
-console.log( 10 + b * a / 4 );
+
+console.log('a =', a )
+console.log('b =', b )
+console.log('a + b =', a + b)
+console.log('a - b =', a - b)
+console.log('a * b =', a * b)
+console.log('a / b =', a / b)
+console.log('a % b =', a % b)
+console.log('a++ =', a++)
+console.log('++a =', ++a)
+console.log('a-- =', a--)
+console.log('--a =', --a)
+console.log('________________')
+
+// Latihan 5.3. Melakukan operasi sederhana dengan operator perbandingan
+
+
+console.log('a > b =', a > b)
+console.log('a < b =', a < b)
+console.log('a >= b =', a >= b)
+console.log('a <= b =', a <= b)
+console.log('a == b =', a == b)
+console.log('a != b =', a != b)
+console.log('a === b =', a === b)
+console.log('a !== b =', a !== b)
+console.log('________________')
+
+// Latihan 5.4. Melakukan operasi sederhana dengan operator logika
+// console.log('a && a =', true && true)
+// console.log('a && b =', true && false)
+// console.log('b && a =', false && true)
+// console.log('b && b =', false && false)
+// console.log('a || a =', true || true)
+// console.log('a || b =', true || false)
+// console.log('b || a =', false || true)
+// console.log('b || b =', false || false)
+// console.log('!a =', !true)
+// console.log('!b =', !false)
+// console.log('________________')
+
 // menampilkan informasi dalam bentuk alert pada window
 window.alert("Angka favorit saya adalah : " + myNumber)
 window.alert('Angka favorit saya adalah : ' + myNumber)
