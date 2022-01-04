331feddb66c1431ef502d08e8f31a12afe9b0f2f
poineko <eliteisan@gmail.com>
2022-01-04 19:10:07 +0700

    Latihan 5.2. Melakukan operasi sederhana dengan operator aritmatika

--- Tugas5/learn.js
+++ Tugas5/learn.js
@@ -8,7 +8,17 @@ console.log(myNumber)
 console.log(myString)
 console.log(myBoolean)
 console.log(phi)
+
+//Latihan 5.2. Melakukan operasi sederhana dengan operator aritmatika
+var a = 3, b = 6;
+console.log( a - b );
+console.log( 2 - a - b );
+console.log('3 + 6 =', a + b);
+console.log( 10 + b * a / 4 );
 // menampilkan informasi dalam bentuk alert pada window
 window.alert("Angka favorit saya adalah : " + myNumber)
 window.alert('Angka favorit saya adalah : ' + myNumber)
 window.alert(`Angka favorit saya adalah : ${myNumber}`)
+
+
+
